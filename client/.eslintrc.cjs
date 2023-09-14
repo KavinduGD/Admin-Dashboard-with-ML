0844886cdc20d22412a2d67031e2300452aa5f1a
KavinduGD <it21038914@my.sliit.lk>
2023-09-15 02:27:22 +0530

    not fixed back to normal

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,10 +10,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "no-unused-vars": [
-      "error",
-      { vars: "all", args: "after-used", ignoreRestSiblings: false },
-    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },

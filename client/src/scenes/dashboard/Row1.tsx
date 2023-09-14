a0f4982826bf6a3a08d57f4057828fa4a46e4be4
KavinduGD <it21038914@my.sliit.lk>
2023-09-15 01:50:17 +0530

    grid

---
+++ client/src/scenes/dashboard/Row1.tsx
@@ -0,0 +1,17 @@
+import DashBoardBox from "@/components/DashBoardBox";
+
+type Props = {};
+
+const Row1 = (props: Props) => {
+  return (
+    <>
+      <DashBoardBox gridArea="a"></DashBoardBox>
+
+      <DashBoardBox gridArea="b"></DashBoardBox>
+
+      <DashBoardBox gridArea="c"></DashBoardBox>
+    </>
+  );
+};
+
+export default Row1;

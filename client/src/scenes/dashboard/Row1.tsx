f551f99d3a8c85ce6c3102d8df9a5659aec77d71
KavinduGD <it21038914@my.sliit.lk>
2023-09-15 20:21:05 +0530

    backend

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,6 +1,8 @@
 import DashBoardBox from "@/components/DashBoardBox";
+import { useGetKpisQuery } from "@/state/api";
 
 const Row1 = () => {
+  const { data } = useGetKpisQuery();
   return (
     <>
       <DashBoardBox gridArea="a"></DashBoardBox>
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+//mongoose config
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, // Add this option to avoid deprecation warnings
+    useUnifiedTopology: true, // Add this option to avoid deprecation warnings
+  })
+  .then(async () => {
+    app.listen(PORT, () => {
+      console.log(`Server connected Port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });

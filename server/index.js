1c84d311daabe1b30d02f76b661bc7fdef8ea433
KavinduGD <it21038914@my.sliit.lk>
2023-09-16 17:45:57 +0530

    backend frontend works

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 // Configurations
 dotenv.config();
 const app = express();
@@ -20,14 +22,21 @@ app.use(cors());
 //mongoose config
 const PORT = process.env.PORT || 9000;
 
+//Routes
+app.use("/kpi", kpiRoutes);
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true, // Add this option to avoid deprecation warnings
     useUnifiedTopology: true, // Add this option to avoid deprecation warnings
   })
   .then(async () => {
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
       console.log(`Server connected Port : ${PORT}`);
+
+      //added one time
+      // await mongoose.connection.db.dropDatabase();
+      // KPI.insertMany(kpis);
     });
   })
   .catch((error) => {
